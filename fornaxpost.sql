ae79414ac0b0c158c89f2ddcea2b129ff79971ee
Lucas Quaroni <lucas.quaroni@enerminds.com>
2023-10-23 14:42:24 -0300

    new database cleaned

--- fornaxpost.sql
+++ fornaxpost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2023 a las 04:38:48
+-- Tiempo de generación: 23-10-2023 a las 19:41:49
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -38,16 +38,10 @@ CREATE TABLE `artefactos` (
 --
 
 INSERT INTO `artefactos` (`serial`, `modelo`, `garantia`) VALUES
-('12321', NULL, NULL),
-('12989898', 'FORNAX FIT 50', 'S'),
-('2123132', 'asd', 'S'),
-('23132132131', 'FORNAX FIT 50', 'S'),
-('2313213999', NULL, NULL),
-('434', 'modelo_artefacto', ''),
-('4344', 'asd prueba 1', 'S'),
-('44442', NULL, NULL),
-('444422131', NULL, NULL),
-('444422131323', NULL, NULL);
+('123456', 'FORNAX FIT 50', 'N'),
+('456789', 'FORNAX RISTORANTE', 'N'),
+('888888', 'FORNAX FREIDORA', 'N'),
+('999999', 'FORNAX CLASSIC', 'N');
 
 -- --------------------------------------------------------
 
@@ -72,14 +66,8 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`dni`, `nombreYapellido`, `domicilio`, `telefono`, `email`, `provincia`, `localidad`, `codpostal`, `obs`) VALUES
-('1', 'yo', 'asd', 'asd', 'asda@gmail.com', 'sda', 'asd', 'ads', 'asdada'),
-('22223', 'marvo', 'asd', 'asd', 'asd@gmail.com', 'asd', '|asd', 'ads', 'asdsa'),
-('34243', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Cliente nuevo'),
-('44232238', 'Marcelo Benitez', 'Balbo 4156', '3415690470', 'marcebenitez0607@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Marcelo el nene'),
-('44523085', 'Emma Procaccini', 'Avenida del huerto 1223', '3415690470', 'emmaproca@icloud.com', 'Santa Fe', 'Rosario', '2000', 'Emma nueva cliente top'),
-('44765222', 'asd', 'asd', 'asd', 'asda@gmail.com', 'asntasda', 'dsa', 'dsa', 'dsasda'),
-('44765283', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Nada que observar'),
-('888', 'Maria', 'adad', '232213', 'dsadsa@gmail.com', 'dsadsa', 'dadqs', 'dadwa', 'dsadwa');
+('44232238', 'Marcelo Benitez', 'Balbo 4156', '3415690470', 'marcebenitez0607@gmail.com', 'Santa Fe', 'Rosario', '2000', 'cortada entre garibaldi y chuquisaca'),
+('44765283', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Casa de portón negro, frente a la estación de servicio.');
 
 -- --------------------------------------------------------
 
@@ -92,6 +80,14 @@ CREATE TABLE `estados` (
   `nombre` varchar(15) DEFAULT NULL,
   `descripcion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estados`
+--
+
+INSERT INTO `estados` (`idestado`, `nombre`, `descripcion`) VALUES
+('ASI', 'asignado', 'reclamo asignado a un responsable'),
+('PEN', 'pendiente', 'reclamo pendiente de asignación');
 
 -- --------------------------------------------------------
 
@@ -130,42 +126,11 @@ CREATE TABLE `reclamos` (
 --
 
 INSERT INTO `reclamos` (`id`, `dni`, `fecha`, `serial`, `idadmin`, `descripcion`, `idestado`) VALUES
-(5, '44765283', '2023-10-14', '23132132131', NULL, 'prueba', NULL),
-(6, '44765283', '2023-10-14', '12989898', NULL, 'prueba', NULL),
-(7, '44765283', '2023-10-14', '12989898', NULL, 'prueba', NULL),
-(8, '44765283', '2023-10-14', '12989898', NULL, 'prueba', NULL),
-(9, '44765283', '2023-10-14', '12989898', NULL, 'prueba', NULL),
-(10, '44765283', '2023-10-14', '12989898', NULL, 'prueba dos', NULL),
-(11, '44765283', '2023-10-14', '12989898', NULL, 'prueba dos', NULL),
-(12, '44765283', '2023-10-14', '12989898', NULL, 'prueba 3', NULL),
-(13, '44765283', '2023-10-14', '12989898', NULL, 'prueba 15', NULL),
-(14, '44765283', '2023-10-14', '12989898', NULL, 'prueba 16', NULL),
-(15, '44765283', '2023-10-14', '12989898', NULL, 'prueba 12', NULL),
-(16, '44765283', '2023-10-14', '12989898', NULL, 'prueba 114', NULL),
-(17, '44765283', '2023-10-14', '12989898', NULL, 'prueba 20', NULL),
-(18, '44765283', '2023-10-14', '12989898', NULL, 'sdadsadsa', NULL),
-(19, '44765283', '2023-10-14', '12321', NULL, 'prueba definitiva', NULL),
-(20, '44765283', '2023-10-15', '12989898', NULL, 'Prueba nueva, numero 20', NULL),
-(21, '44765283', '2023-10-15', '12989898', NULL, 'se me revento la puerta', NULL),
-(22, '44765283', '2023-10-15', '12321', NULL, 'no anda', NULL),
-(23, '44765283', '2023-10-15', '12989898', NULL, 'jhonathan', NULL),
-(24, '44765283', '2023-10-15', '12321', NULL, 'revento la puerta', NULL),
-(25, '44765283', '2023-10-16', '23132132131', NULL, 'prueba numero nose cuanto, 16/10', NULL),
-(26, '44765283', '2023-10-16', '12989898', NULL, 'aliexpress', NULL),
-(27, '44765283', '2023-10-16', '23132132131', NULL, 'prueba', NULL),
-(28, '44765283', '2023-10-16', '23132132131', NULL, 'revento', NULL),
-(29, '44765283', '2023-10-16', '12989898', NULL, 'nada, es una prueba', NULL),
-(30, '34243', '2023-10-16', '12989898', NULL, 'ensimismada', NULL),
-(31, '44765222', '2023-10-16', '2313213999', NULL, 'dsadsadas', NULL),
-(32, '44765222', '2023-10-16', '2313213999', NULL, 'dsadsadas', NULL),
-(33, '1', '2023-10-16', '44442', NULL, 'se rompio la chaveta', NULL),
-(34, '1', '2023-10-16', '444422131', NULL, 'se rompio la chaveta', NULL),
-(35, '1', '2023-10-16', '444422131', NULL, 'se rompio la chaveta', NULL),
-(36, '1', '2023-10-16', '444422131323', NULL, 'se rompio la chaveta', NULL),
-(37, '1', '2023-10-16', '434', NULL, 'a ver si carga bien la cocina', NULL),
-(38, '1', '2023-10-16', '4344', NULL, 'a ver si carga bien la cocina', NULL),
-(39, '44765283', '2023-10-17', '44442', NULL, 'prueba final antes del marvo', NULL),
-(40, '44765283', '2023-10-18', '2123132', NULL, 'asd', NULL);
+(75, '44765283', '2023-10-23', '123456', 3, 'Se me revento la puerta del horno', 'PEN'),
+(76, '44232238', '2023-10-23', '456789', 2, 'Las hornallas se me fundieron', 'ASI'),
+(77, '44765283', '2023-10-23', '123456', 1, 'Se me termino la garantía, puedo extenderla?', 'PEN'),
+(78, '44765283', '2023-10-23', '888888', 1, 'Pierde aceite', 'PEN'),
+(79, '44232238', '2023-10-23', '999999', 1, 'Las perillas se me descascaran', 'PEN');
 
 -- --------------------------------------------------------
 
@@ -274,7 +239,7 @@ ALTER TABLE `fletes`
 -- AUTO_INCREMENT de la tabla `reclamos`
 --
 ALTER TABLE `reclamos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
