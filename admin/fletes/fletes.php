b8aed3663089500b799e132b5bc18f2bf8f6fdf1
Lucas Quaroni <lucas.quaroni@enerminds.com>
2023-10-25 11:42:26 -0300

    control de variables de sesión

---
+++ admin/fletes/fletes.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['usuario'])) {
+  header("Location: ../login/login.php");
+  exit;
+}
+?>
--- admin/reclamos/reclamos.php
+++ admin/reclamos/reclamos.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['usuario'])) {
+  header("Location: ../login/login.php");
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
